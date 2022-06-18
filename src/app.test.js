70dcf4dfbe0c907c83884e5e43051b4f9e92e70e
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-06-18 07:27:16 -0700

    Updated to Hello Class

--- src/app.test.js
+++ src/app.test.js
@@ -6,6 +6,6 @@ describe("/test endpoint", () => {
     it("should return a response", async () => {
         const response = await request.get("/")
         expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello world");
+        expect(response.text).toBe("Hello Class!");
     })
 })
