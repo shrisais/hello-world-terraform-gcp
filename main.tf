df71919244d244f13d29c9cae899dfd2ccb70cda
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-06-22 19:37:24 -0700

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "hello-world-cicd-bucket"
-  location = "us-west2"
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("/Users/shri/Documents/Auth/gcp-credential.json")
+  project = "focus-poet-354200"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
