3abb8e0b64ae465fab07fb502829a9600af4c339
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-06-22 19:49:18 -0700

    Updated main.tf

--- main.tf
+++ main.tf
@@ -8,14 +8,9 @@ terraform {
 }
 
 provider "google" {
-  credentials = var.gcp-creds
   project = "focus-poet-354200"
   region  = "us-central1"
   zone    = "us-central1-c"
-}
-
-variable "gcp-creds" {
-  default= ""
 }
 
 resource "google_compute_network" "vpc_network" {
