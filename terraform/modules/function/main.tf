d80eb21a0e7625171ed860b9bb659dc97755682e
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-06-29 15:57:52 -0700

    Updated runtime

--- terraform/modules/function/main.tf
+++ terraform/modules/function/main.tf
@@ -45,7 +45,7 @@ resource "google_project_service" "cb" {
 # Create Cloud Function
 resource "google_cloudfunctions_function" "function" {
   name    = var.name
-  runtime = "nodejs18"
+  runtime = "nodejs12"
 
   available_memory_mb   = 128
   source_archive_bucket = google_storage_bucket.bucket.name
