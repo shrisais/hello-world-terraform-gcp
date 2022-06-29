79640e3ebf60c37f360c46d09a067120674e1eb0
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-06-29 15:54:55 -0700

    Updated members

--- terraform/modules/function/main.tf
+++ terraform/modules/function/main.tf
@@ -61,7 +61,5 @@ resource "google_cloudfunctions_function_iam_member" "invoker" {
   cloud_function = google_cloudfunctions_function.function.name
 
   role   = "roles/cloudfunctions.invoker"
-  members = [
-    "allUsers",
-  ]
+  member = "allUsers"
 }
