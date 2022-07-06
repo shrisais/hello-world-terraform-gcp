d5d0bf042cb2d0e42ae05989ab9392302db85eb1
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-07-05 22:46:53 -0700

    Added FaunaDB config

--- src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.disable("x-powered-by");
-
-app.get("/", (_req, res) =>  {
-    res.status(200).send("Hello Class!")
-})
-module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-const fastify = require('fastify')
-const app = fastify({ logger: true })
+const fastify = require('fastify')({ logger: true });
+const FaunaError = require('./errors/FaunaError.js');
 
-app.get('/', async (req, res) => {
-    return { works: true }
-})
+// Routes
+fastify.post('/login', require('./routes/login.js'));
+fastify.post('/logout', require('./routes/logout.js'));
+fastify.post('/users', require('./routes/create-user.js'));
+fastify.get('/users/:userId', require('./routes/get-user.js'));
+fastify.delete('/users/:userId', require('./routes/delete-user.js'));
 
-app.get('/hi', async (req, res) => {
-    return "Hello Class!"
-})
+// Hooks
+fastify.addHook('onRequest', async (request, reply) => {
 
-exports.app = async (req, res) => {
-    await app.ready()
-    app.server.emit('request', req, res)
-}
+    // If the route is not private we ignore this hook
+    if (!reply.context.config.isPrivate) return;
+
+    const faunaSecret = request.headers['fauna-secret'];
+
+    // If there is no header
+    if (!faunaSecret) {
+        reply.status(401).send();
+        return;
+    }
+
+    // Add the secret to the request object
+    request.faunaSecret = faunaSecret;
+});
+
+// Decorators
+fastify.decorateRequest('faunaSecret', '');
+
+async function start () {
+    try {
+        await fastify.listen(3000);
+        fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1);
+    }
+};
+
+exports.app = start();
