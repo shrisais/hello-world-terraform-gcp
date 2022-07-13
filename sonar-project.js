464dc8f07ffd39d9ba5efb9508f46c7b86354ff1
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-07-13 09:00:43 -0700

    Updated sonar

--- sonar-project.js
+++ sonar-project.js
@@ -3,7 +3,7 @@ sonarqubeScanner(
     {
         serverUrl:  'http://localhost:9000',
         options : {
-            'sonar.projectKey': 'hello-world-app-ci-cd',
+            'sonar.projectKey': 'shrisais_hello-world-terraform-gcp_AYH4RJ1VpHi5BpJq1EPb',
             'sonar.sources':  'src',
             'sonar.tests':  'src',
             'sonar.inclusions'  :  '**', // Entry point of your code
