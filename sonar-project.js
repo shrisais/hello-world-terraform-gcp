69f38c18e014d304f4aafe19fb63907569e10a1f
Shri Sai Sadhana Natarajan <shrisais@users.noreply.github.com>
2022-07-13 09:08:38 -0700

    Updated sonar

--- sonar-project.js
+++ sonar-project.js
@@ -1,3 +1,4 @@
+/*
 const sonarqubeScanner =  require('sonarqube-scanner');
 sonarqubeScanner(
     {
@@ -7,8 +8,8 @@ sonarqubeScanner(
             'sonar.sources':  'src',
             'sonar.tests':  'src',
             'sonar.inclusions'  :  '**', // Entry point of your code
-            'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+            'sonar.test.inclusions':  'src/!**!/!*.spec.js,src/!**!/!*.spec.jsx,src/!**!/!*.test.js,src/!**!/!*.test.jsx',
             'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
             'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml'
         }
-    }, () => {});
+    }, () => {});*/
